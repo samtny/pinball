01f00954b8ebb4f8770fb7411816198045bb9d7e
Sam T <samtny@gmail.com>
2012-12-14 12:54:07 -0500

    minor tweaks to flipperfall layout

--- shared/Flipperfall/forces.lua
+++ shared/Flipperfall/forces.lua
@@ -13,7 +13,7 @@ forces = {
 	slingshotRestLength = 0.6,
 	slingshotSwitchGap = 0.25,
 	slingshotStiffness = 15.0,
-	slingshotDamping = 0.35,
+	slingshotDamping = 0.9,
 	slingshotImpulse = 0.05,
 
 	targetStiffness = 35.0, -- glass == 90
