359024d21484c877d4f52876a3d2c476c5cc46b3
Sam T <samtny@gmail.com>
2012-12-31 15:48:17 -0500

    mid-refactor

--- shared/Part.h
+++ shared/Part.h
@@ -1,5 +1,16 @@
 
+struct Material;
+struct Texture;
+
 typedef struct Part {
 	string n;
-
+	int v;
+	string s;
+	float r1;
+	float r2;
+	Material *m;
+	Texture *t;
 } Part;
+typedef map<string, Part>::iterator it_Part;
+
+
