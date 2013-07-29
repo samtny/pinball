c5ea78c72f72019beeafe9aa50e5d93b07ddea1d
Sam T <samtny@gmail.com>
2013-07-28 21:47:10 -0400

    begin CatBurglar whitewood

---
+++ shared/resource/CatBurglar/camera.lua
@@ -0,0 +1,20 @@
+
+
+camera = {
+
+	centered = {
+		t = "fixed",
+		z = 1.0,
+		c = { 10, 20 },
+		w = 40
+	},
+    
+	free = {
+		t = "follow",
+		z = 1.0,
+		b = { 0.5, 0.35 }, -- buffers currently expressed as percent of viewable x/y
+		w = 10.0436
+	}
+
+}
+
---
+++ shared/resource/CatBurglar/forces.lua
@@ -0,0 +1,33 @@
+
+forces = {
+
+	gravityBase = { 0.0, -9.80665 },
+	tableAngle = 6.5,
+	gravity = { 0.0, -9.80665 * ( 10.5 / 90.0 ) },
+	
+	boxStiffness = 500.0, -- steel = 200
+	boxDamping = 70.0,
+
+	flipImpulse = 0.005,
+	flipForce = 0.01,
+	unflipImpulse = 0.0,
+	unflipForce = 0.01,
+
+	slingshotRestLength = 0.6,
+	slingshotSwitchGap = 0.35,
+	slingshotStiffness = 55.0,
+	slingshotDamping = 0.5,
+	slingshotImpulse = 0.007,
+
+	targetStiffness = 190.0, -- glass == 90
+	targetDamping = 0.50,
+	targetRestLength = 0.9,
+	targetSwitchGap = 0.25,
+
+	popBumperThreshold = 0.005,
+	popBumperImpulse = 0.0055,
+
+	nudgeImpulse = 0.1
+
+}
+
