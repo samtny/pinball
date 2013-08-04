49549bb076e9e8d7f3013607f692807dfdb769a1
Sam T <samtny@gmail.com>
2013-08-03 21:32:51 -0400

    getting closer.

--- shared/resource/CatBurglar/forces.lua
+++ shared/resource/CatBurglar/forces.lua
@@ -3,15 +3,16 @@ forces = {
 
 	gravityBase = { 0.0, -9.80665 },
 	tableAngle = 6.5,
-	gravity = { 0.0, -9.80665 * ( 6.5 / 90.0 ) },
+	gravity = { 0.0, -9.80665 * ( 12.5 / 90.0 ) },
 	
-	boxStiffness = 20.0, -- steel = 200
-	boxDamping = 5.0,
+	boxStiffness = 10000, -- steel = 200
+	boxDamping = 1000,
 
-	flipImpulse = 0.006,
-	flipForce = 0.25,
-	unflipImpulse = 0.005,
-	unflipForce = 0.05,
+	flipImpulse = 0.1,
+	flipForce = 5,
+
+	unflipImpulse = 0.1,
+	unflipForce = .25,
 
 	slingshotRestLength = 0.6,
 	slingshotSwitchGap = 0.35,
@@ -24,10 +25,10 @@ forces = {
 	targetRestLength = 0.125,
 	targetSwitchGap = 0.05,
 
-	popBumperThreshold = 0.005,
-	popBumperImpulse = 10.1055,
+	popBumperThreshold = 0,
+	popBumperImpulse = 0.1,
 
-	nudgeImpulse = 0.1
+	nudgeImpulse = 30
 
 }
 
--- shared/resource/CatBurglar/materials.lua
+++ shared/resource/CatBurglar/materials.lua
@@ -5,7 +5,7 @@ materials = {
 	ball = { e=0.999999, f=0.999999, d=7.85 },
 	wood = { e=0.20, f=0.65, d=0.53 },
 	rubber = { e=0.50, f=0.90, d=1.1 },
-	flipper = { e=0.45, f=0.39, d=1.2 },
+	flipper = { e=0.55, f=0.39, d=1.2 },
 	wire = { e=0.30, f=0.10, d=7.85 },
 	steel = { e=0.10, f=0.10, d=7.85 },
 	plastic = { e=0.30, f=0.20, d=1.2} -- acrylic == 1.2
