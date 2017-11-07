dc90c5f8197e53857d9943ca908c99c703e9f53b
Sam Thompson <sthompson@digitalpulp.com>
2017-11-07 16:28:42 -0500

    lua simulator build

---
+++ ios/libLua/lua/lua/lua.h
@@ -0,0 +1,13 @@
+//
+//  lua.h
+//  lua
+//
+//  Created by Sam Thompson on 11/7/17.
+//  Copyright © 2017 Sam Thompson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface lua : NSObject
+
+@end
---
+++ ios/libLua/lua/lua/lua.m
@@ -0,0 +1,13 @@
+//
+//  lua.m
+//  lua
+//
+//  Created by Sam Thompson on 11/7/17.
+//  Copyright © 2017 Sam Thompson. All rights reserved.
+//
+
+#import "lua.h"
+
+@implementation lua
+
+@end
