96f08a1e0abf056ec41fd91f660c3a5720620916
Sam Thompson <sthompson@digitalpulp.com>
2017-11-10 10:56:49 -0500

    fix gravity

--- shared/resource/CatBurglar/forces.lua
+++ shared/resource/CatBurglar/forces.lua
@@ -3,7 +3,7 @@ forces = {
 
 	gravityBase = { 0.0, -9.80665 },
 	tableAngle = 6.5,
-	gravity = { 0.0, -9.80665 * ( 16.5 / 90.0 ) },
+	gravity = { 0.0, -9.80665 * math.sin(6.5 * (6.5 / 180)) },
 	
 	boxStiffness = 10000, -- steel = 200
 	boxDamping = 1000,
@@ -12,7 +12,7 @@ forces = {
 	flipForce = 8,
 
 	unflipImpulse = 0.1,
-	unflipForce = .25,
+	unflipForce = 3,
 
 	slingshotRestLength = 0.4,
 	slingshotSwitchGap = 0.3,
