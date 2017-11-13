49ebdfbcaa43620cfa63e9f655a1bd3d841f0db3
Sam Thompson <sthompson@digitalpulp.com>
2017-11-13 17:17:32 -0500

    initial setup

--- shared/resource/CatBurglar/materials.lua
+++ shared/resource/CatBurglar/materials.lua
@@ -2,10 +2,10 @@
 
 materials = {
 	box = { e=0.20, f=0.65, d=1.06}, -- 2 layers of wood, perhaps
-	ball = { e=0.999999, f=0.999999, d=7.85 },
+	ball = { e=0.999999, f=0.999999, d=8.05 },
 	wood = { e=0.20, f=0.65, d=0.53 },
 	rubber = { e=0.70, f=0.20, d=1.1 },
-	flipper = { e=0.55, f=0.20, d=1.2 },
+	flipper = { e=0.5, f=0.31, d=1.2 },
 	wire = { e=0.30, f=0.10, d=7.85 },
 	steel = { e=0.10, f=0.10, d=7.85 },
 	plastic = { e=0.30, f=0.20, d=1.2} -- acrylic == 1.2
